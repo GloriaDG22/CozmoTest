28c4376d6413e3eca9429a94058924b72c07f9a3
GloriaDG22 <49491746+GloriaDG22@users.noreply.github.com>
2021-10-09 15:00:49 +0200

    Update ShowImage.py
    With parameters

--- ShowImage.py
+++ ShowImage.py
@@ -3,46 +3,54 @@ import time
 from PIL import Image
 import cozmo
 
+def get_in_position(robot: cozmo.robot.Robot):
+    '''If necessary, Move Cozmo's Head and Lift to make it easy to see Cozmo's face'''
+    if (robot.lift_height.distance_mm > 45) or (robot.head_angle.degrees < 40):
+        with robot.perform_off_charger():
+            lift_action = robot.set_lift_height(0.0, in_parallel=True)
+            head_action = robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE,
+                                               in_parallel=True)
+            lift_action.wait_for_completed()
+            head_action.wait_for_completed()
+
+def readFile():
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    param_path = os.path.join(current_directory, "cozmo_sdk_examples_1.4.10/parameters_file", "parameter_show_image.txt")
+    file = open(param_path, "r")
+    
+    line = file.readline().split(";")
+    return line
 
 def show_image(robot: cozmo.robot.Robot):
-    
-    robot.set_head_angle(degrees(45.0))
-    robot.set_lift_height(0.0)
+    line = readFile()
 
+    get_in_position(robot)
     current_directory = os.path.dirname(os.path.realpath(__file__))
-    image_path = os.path.join(current_directory, "..", "face_images", "Glowinn_logo.png")
+    image_path = os.path.join(current_directory, "cozmo_sdk_examples_1.4.10/face_images", line[0])
 
-    # Cargamos la imagen
-    image_file = Image.open(image_path)
+    # load some images and convert them for display cozmo's face
+    face_images = []
+    image = Image.open(image_path)
 
-    # Convertimos la imagen a pixels en la pantalla
-    resized_image = image_file.resize(cozmo.oled_face.dimensions(), Image.NEAREST)
-    
+    # resize to fit on Cozmo's face screen
+    resized_image = image.resize(cozmo.oled_face.dimensions(), Image.BICUBIC)
+
     # convert the image to the format used by the oled screen
     face_image = cozmo.oled_face.convert_image_to_screen_data(resized_image, invert_image=True)
     face_images.append(face_image)
 
-    # Truco para quitar los ojos
-    # robot.screen.set_screen_to_color(anki_vector.color.off, duration_sec=0.1)
-
-    # Mostramos la imagen 5 segundos
-    robot.screen.display_oled_face_image(face_image, 10.0)
+    duration_s = 2.0
+    for image in face_images:
+        robot.display_oled_face_image(image, 5000.0)
+        time.sleep(duration_s)
 
     # Sonido
-    robot.say_text("Hello Glowiiiiinnnn!")
-    time.sleep(0.7)
+    robot.say_text(line[1].replace('\n',''), in_parallel=True)
+    
     # Golpe con la palanca!
-    robot.motors.set_lift_motor(5)
-    time.sleep(0.1)
-    robot.motors.set_lift_motor(3)
-    time.sleep(0.1)
-    robot.motors.set_lift_motor(5)
-    time.sleep(0.1)
-    robot.motors.set_lift_motor(3)
-    time.sleep(0.1)
-    robot.motors.set_lift_motor(5)
-    time.sleep(0.5)
-    robot.motors.set_lift_motor(0)
-
+    robot.set_lift_height(1, accel=50.0, in_parallel=True).wait_for_completed()
+    robot.set_lift_height(0.3, accel=20.0, in_parallel=True).wait_for_completed()
+    robot.set_lift_height(1, accel=20.0,in_parallel=True).wait_for_completed()
+    robot.set_lift_height(0.0, accel=50.0,in_parallel=True).wait_for_completed()
 
 cozmo.run_program(show_image)
